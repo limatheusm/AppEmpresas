036875de00d212784f248b80b1111861b4b98f3f
Matheus Lima <limatheusm@gmail.com>
2020-09-25 20:46:08 -0300

    Initial xcode setup

---
+++ AppEmpresas/AppEmpresas/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  AppEmpresas
+//
+//  Created by Matheus Lima on 25/09/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ AppEmpresas/AppEmpresas/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  AppEmpresas
+//
+//  Created by Matheus Lima on 25/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
